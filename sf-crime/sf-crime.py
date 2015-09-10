dc665c103114e2ddc8456c297a6bbd87525ccf51
wqslucifer <qslucifer@gmail.com>
2015-09-10 16:33:56 -0400

    solved split problem

--- sf-crime/sf-crime.py
+++ sf-crime/sf-crime.py
@@ -12,30 +12,39 @@ train_data = pd.read_csv("C:\\Users\\qiushi\OneDrive\kaggle\sf-crime\\train.csv"
 test_data = pd.read_csv("C:\\Users\\qiushi\OneDrive\kaggle\sf-crime\\test.csv")
 
 
-def transform_date(train_date):
-    date_int = pd.DataFrame(columns=['year', 'month', 'day', 'time'])
-    row, col = train_data.shape
-    j = 0
-    for i in train_date:
-        print("job=", round(j / row * 100, 2), "%")
-        j += 1
-        date, time = i.split(' ')
-        y, m, d = date.split('-')
-        time = time.split(':')[:2]
-        time = int(time[0]) * 60 + int(time[1])
-        date_int = date_int.append(pd.Series([y, m, d, time], index=['year', 'month', 'day', 'time']),
-                                   ignore_index=True)
-    return date_int
+def transform_date(train):
+    # separate the date to year, month, date and time in int
+    df_date_time = train.Dates.str.split(' ')
+    df_date = df_date_time.apply(lambda x: x[0])
+    df_time = df_date_time.apply(lambda x: x[1])
+
+    df_sp_date = df_date.str.split('-')
+    df_year_int = df_sp_date.apply(lambda x: int(x[0]))
+    df_month_int = df_sp_date.apply(lambda x: int(x[1]))
+    df_date_int = df_sp_date.apply(lambda x: int(x[2]))
+
+    df_sp_time = df_time.str.split(':')
+    df_hours_int = df_sp_time.apply(lambda x: int(x[0]))
+    df_minutes_int = df_sp_time.apply(lambda x: int(x[1]))
+    df_seconds_int = df_sp_time.apply(lambda x: int(x[2]))
+    time_int = df_sp_time.apply(lambda x: int(x[0]) * 60 + int(x[1]))
+    # time is in minutes without seconds
+    date_int = pd.DataFrame({'Year': df_year_int, 'Month': df_month_int,
+                             'Date': df_date_int, 'Time': time_int})
+    train = date_int.join(train.drop('Dates', axis=1), how='outer')
+
+    # separate address into category
+    train.Address.apply(lambda x: x.split('/') if '/' in x else x.split('of'))
+
+    return train
 
 
 # Recode categories to numerical
-X = pd.DataFrame()
-y = pd.Categorical.from_array(train_data['Category']).codes
-X.DayOfWeek = pd.Categorical.from_array(train_data['DayOfWeek']).codes
-X.PdDistrict = pd.Categorical.from_array(train_data['PdDistrict']).codes
-print("transfor date...")
-date = transform_date(train_data.Dates)
-print(date[:10])
+train_data.Category = pd.Categorical.from_array(train_data['Category']).codes
+train_data.DayOfWeek = pd.Categorical.from_array(train_data['DayOfWeek']).codes
+train_data.PdDistrict = pd.Categorical.from_array(train_data['PdDistrict']).codes
+X = transform_date(train_data)
+print(X[:10])
 '''
 vectorizer = DictVectorizer(sparse=False)
 vec_train = vectorizer.fit_transform(category_dict)
